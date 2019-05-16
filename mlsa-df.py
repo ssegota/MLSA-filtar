bcd1f2e64637e3a747d6356eca30ae9d0e366794
Sandi <ssegota@gmail.com>
2019-05-17 00:01:00 +0200

    Working filter implementation

---
+++ mlsa-df.py
@@ -0,0 +1,66 @@
+
+from pysptk.synthesis import MLSADF, Synthesizer
+import seaborn
+import numpy as np
+import pysptk
+from scipy.io import wavfile
+import librosa
+import librosa.display
+import matplotlib
+import seaborn
+import time
+from matplotlib import pyplot as plt
+seaborn.set(style="whitegrid")
+
+frame_length = 256
+hop_length = 10
+
+alpha = 0.41
+sr, x = wavfile.read(pysptk.util.example_audio_file())
+
+#assert sr == 16000
+x = x.astype(np.float64)
+print(x.shape)
+wavfile.write("x.wav", sr, x)
+
+f = open("times", "w")
+f.write("order,time\n")
+for order in (0,4,9,14,24,49):
+    start = time.time()
+    # Note that almost all of pysptk functions assume input array is C-contiguous and np.float64 element type
+    frames = librosa.util.frame(
+        x, frame_length=frame_length, hop_length=hop_length).astype(np.float64).T
+
+    # Windowing
+    frames *= pysptk.blackman(frame_length)
+
+    assert frames.shape[1] == frame_length
+
+    pitch = pysptk.swipe(x.astype(np.float64), fs=sr,
+                        hopsize=hop_length, min=60, max=240, otype="pitch")
+    source_excitation = pysptk.excite(pitch, hop_length)
+
+
+    # Order of mel-cepstrum
+
+
+    mc = pysptk.mcep(frames, order, alpha)
+    logH = pysptk.mgc2sp(mc, alpha, 0.0, frame_length).real
+    print(mc.shape)
+    #plt.plot(mc)
+    #plotname="x_syn_coefs_" + str(order) + ".png"
+    #plt.savefig(plotname)
+
+    # Convert mel-cesptrum to MLSADF coefficients
+    b = pysptk.mc2b(mc, alpha)
+
+    synthesizer = Synthesizer(MLSADF(order=order, alpha=alpha), hop_length)
+
+    x_synthesized = synthesizer.synthesis(source_excitation, b)
+
+    filenam = "synthesized_sounds/"+"x_syn" + str(order+1) + ".wav"
+    #wavfile.write("x.wav", sr, x)
+    wavfile.write(filenam,sr,x_synthesized)
+    time_total = time.time() - start
+    writestring = str(order)+","+str(time_total)+"\n"
+    f.write(writestring)
---
+++ x1/compare.py
@@ -0,0 +1,34 @@
+from scipy.io import wavfile
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.integrate import simps
+from math import log10
+
+
+
+sr, x = wavfile.read("x.wav")
+assert sr == 16000
+x = x.astype(np.float64)
+print(x.shape)
+
+
+for i in (0, 1, 2, 4, 9, 14, 24, 49, 74, 99):
+
+    filename_syn = "x_syn" + str(i+1) + ".wav"
+    sr, x5 = wavfile.read("synthesized_sounds/"+filename_syn)
+    assert sr == 16000
+    x5 = x5.astype(np.float64)
+    print(x5.shape)
+    diff = []
+
+    for i in range(len(x5)):
+        dif = abs(x[i]-x5[i])
+        if dif > 0:
+            diff.append(10*log10(dif))
+        else:
+            diff.append(0)
+    plt.figure()
+    plt.plot(diff)
+    plotname = "differences/"+filename_syn[:-4] + ".png"
+    plt.savefig(plotname)
+
